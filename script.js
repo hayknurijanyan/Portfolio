eba3e158c031d688be0f53c799cebff49c237d8f
Hayk <hayknurijanyan@gmail.com>
2021-02-25 18:44:35 +0400

    animation changes

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ console.log($myProject1)
 projectList.each(function (){
   
   $(this).mouseover(function() {
-    if ($(this).css("margin")=== "0px"){
+    if ($(this).css("margin")=== "15px"){
       $(this).css({"cursor": "pointer"})
-      $(this).animate({"margin": "20px"})
+      $(this).animate({"margin": "0px"})
     } else($(this).stop)
   });
   $(this).mouseout(function() {
-    $(this).animate({"margin": "0px"})
+    $(this).animate({"margin": "15px"})
     
   });
 })
